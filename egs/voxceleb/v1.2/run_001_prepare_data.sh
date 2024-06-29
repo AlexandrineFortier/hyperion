44650ac3d7956204b7bc4bfa4c42dec618c9dae7
AlexandrineFortier <alexandrine.fortier.1@ens.etsmtl.ca>
2024-06-29 16:56:38 -0400

    Delete useless data prep

--- egs/voxceleb/v1.2/run_001_prepare_data.sh
+++ egs/voxceleb/v1.2/run_001_prepare_data.sh
@@ -10,7 +10,7 @@ set -e
 stage=1
 config_file=default_config.sh
 
-. parse_options.sh || exit 1;
+#. parse_options.sh || exit 1;
 . datapath.sh 
 . $config_file
 
@@ -21,26 +21,3 @@ if [ $stage -le 1 ];then
 			--output-dir data/voxceleb2cat_train
 fi
 
-if [ $stage -le 2 ];then
-  # prepare voxceleb1 for test
-  hyperion-prepare-data voxceleb1 --task test --corpus-dir $voxceleb1_root \
-			--use-kaldi-ids \
-			--output-dir data/voxceleb1_test
-fi
-
-if [ $stage -le 3 ] && [ "$do_voxsrc22" == "true" ];then
-  hyperion-prepare-data voxsrc22 --subset dev --corpus-dir $voxsrc22_root \
-			--vox1-corpus-dir $voxceleb1_root \
-			--output-dir data/voxsrc22_dev
-fi
-
-# if [ $stage -le 4 ] && [ "$do_voxsrc22" == "true" ];then
-  #   hyperion-prepare-data voxsrc22 --subset test --corpus-dir $voxsrc22_root \
-  # 		  --vox1-corpus-dir $voxceleb1_root \
-  # 		  --output-dir data/voxsrc22_test
-# fi
-
-if [ $stage -le 5 ] && [ "$do_qmf" == "true" ];then
-  # split vox2 into 2 parts, for cohort and qmf training
-  hyperion-split-dataset-into-trials-and-cohort --data-dir data/voxceleb2cat_train
-fi
